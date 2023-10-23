9034952c653d4de3c2e1cffbe6c71ab18417a3d4
D3bo <debona.giovanni@gmail.com>
2023-10-23 03:49:31 +0200

    display tenants

--- BlazorApp2/BlazorApp2/Data/ApplicationUser.cs
+++ BlazorApp2/BlazorApp2/Data/ApplicationUser.cs
@@ -5,7 +5,6 @@ namespace BlazorApp2.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        public string TenantId { get; set; } = null!;
     }
 
 }
